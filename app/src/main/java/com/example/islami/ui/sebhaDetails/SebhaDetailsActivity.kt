09696b1b3442217b3c58d5de625c144f165fa187
maraim <mariamelgandour9@gmail.com>
2024-08-28 18:27:45 +0300

    feat : sebha Fragment feat : sebha details activity

---
+++ app/src/main/java/com/example/islami/ui/sebhaDetails/SebhaDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.islami.ui.sebhaDetails
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.islami.R
+
+class SebhaDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sebha_details)
+
+    }
+}
